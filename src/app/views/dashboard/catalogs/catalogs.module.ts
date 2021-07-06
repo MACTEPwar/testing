fbf370bbe2273811c3e9ea4291eb92ff6a56d5cc
Виталий Шебаниц <mactepwar@outlook.com>
2021-07-06 14:35:37 +0000

    fix tabs

--- src/app/views/dashboard/catalogs/catalogs.module.ts
+++ src/app/views/dashboard/catalogs/catalogs.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CatalogsRoutingModule } from './catalogs-routing.module';
 import { CatalogComponent } from './catalog/catalog.component';
+import { TabRouteLinkModule } from 'src/app/features/ui-components/tab-route-link/tab-route-link.module';
 
 
 @NgModule({
   declarations: [CatalogComponent],
   imports: [
     CommonModule,
-    CatalogsRoutingModule
+    CatalogsRoutingModule,
+    TabRouteLinkModule
   ]
 })
 export class CatalogsModule { }
